a80553772bb2be25c43dfaf5f4b74a443696e74f
unknown <jonatanesprellalopez@gmail.com>
2024-05-28 19:04:15 -0400

    Ultimos cambios en login

--- routes/Registrar.js
+++ routes/Registrar.js
@@ -4,13 +4,13 @@ const { jsonResponse } = require("../lib/jsonResponsive");
 router.post("/", (req, res) => {
     const {username, name, password} = req.body;
     if(!username || !name || !password  ){
-        return res.status(400).json(jsonResponse(400),{
-            error: "espacios vacios",
-        })
+        return res.status(400).json(jsonResponse(400, {
+            error: "espacios vacios"
+        }));
     }
 
-    res.status(200).json(jsonResponse(200, {message: "Usuario registrado"}));
-    res.send("registrado");
+    res.status(200).json(jsonResponse(200, {message: "Usuario registrado con exito"}));
+    res.send("registrado exito");
 });
 
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,26 @@
 const router = require("express").Router();
+const { jsonResponse } = require("../lib/jsonResponsive");
+
+router.post("/", (req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password  ){
+        return res.status(400).json(jsonResponse(400, {
+            error: "No ingreso algun dato"
+        }));
+    }
 
 
-router.get("/", (req, res) => {
-    res.send ("login");
+//autenticacion
+    const accessToken = "access_token";
+    const refreshToken = "refresh_token";
+    const user = {
+        id: 1,
+        name: 'Jonatan',
+        email: 'XXXXX'
+    }
+
+    res.status(200).json(jsonResponse(200, {user, accessToken, refreshToken}));
+    res.send("logueado exito");
 });
 
 module.exports = router;
